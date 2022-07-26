70c8ec47d560acba5d772b50923f996a874016a1
hmanjun <harshith.hm03@gmail.com>
2022-07-26 12:32:53 -0700

    Add listener for get req on /api/movies

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ const mysql = require('mysql2')
 const PORT = 3001
 const app = express()
 
+app.get('/api/movies', (req,res) => {
 
-
+})
 
 app.listen(PORT, () =>{
     console.log(`Listening on port ${PORT}`)
