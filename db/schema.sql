e77e10cc7d4cb60b964a46ecfa8fc405581c48e3
pbarkley <42686983+pbarkley@users.noreply.github.com>
2022-07-26 14:14:09 -0500

    create movie_db

---
+++ db/schema.sql
@@ -0,0 +1,19 @@
+DROP DATABASE IF EXISTS movie_db;
+CREATE DATABASE movie_db;
+
+USE movie_db;
+
+CREATE TABLE movies (
+    id INT NOT NULL AUTO_INCREMENT,
+    movie_name VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE reviews (
+    id INT NOT NULL AUTO_INCREMENT,
+    movie_id INT,
+    review TEXT NOT NULL,
+    FOREIGN KEY (movie_id)
+    REFERENCES movies (id)
+    ON DELETE SET NULL
+);
--- server.js
+++ server.js
@@ -4,7 +4,18 @@ const mysql = require('mysql2')
 const PORT = 3001
 const app = express()
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      user: 'root',
+      password: 'password',
+      database: 'movie_db'
+    },
+    console.log(`Connected to the movie_db database.`)
+);
 
 
 app.listen(PORT, () =>{
